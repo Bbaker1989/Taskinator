e60d035c246d79373a33af1b41baefae396a4cfe
bbaker1989 <brittneybaker@Brittneys-Air.lan>
2021-09-26 11:24:15 -0400

    updated talk list

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -3,11 +3,14 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) {
   event.preventDefault();
+
+  var taskNameInput = doucment.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
+  
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-console.log(event);
 };
   
 formEl.addEventListener("submit", createTaskHandler);
